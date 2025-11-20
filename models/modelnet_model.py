531adadd48f3b42942fbd89dd8ca26f79c20cca0
Mihirj05 <127704072+Mihirj05@users.noreply.github.com>
2025-11-20 02:34:06 +0000

    add inference code

--- models/modelnet_model.py
+++ models/modelnet_model.py
@@ -1,22 +1,21 @@
-from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.applications.MobileNetV2 import decode_prediction
 import numpy as np
-from .mobilenet_model import MobileNetClassifier
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import decode_predictions
 
-class MobileNetClassfier:
+class MobileNetClassifier:
     def __init__(self):
-        print("Loding of the model.....")
-        self.model = MobileNetV2(weight = 'imagenet')
+        print("Loading the model.....")
+        self.model = MobileNetV2(weights='imagenet')
         print("Model Ready!")
 
     def predict(self, frames):
         if len(frames.shape) == 3:
             frames = np.expand_dims(frames, axis=0)
-        
-        preds = self.model.predict(frames, verbose = 1)
+
+        preds = self.model.predict(frames, verbose=1)
 
         results = []
         for pred in preds:
-            decode = decode_prediction(np.expand_dims(pred,0), top = 5)[0]
+            decode = decode_predictions(np.expand_dims(pred, 0), top=5)[0]
             results.append([(name, float(prob)) for _, name, prob in decode])
         return results
